31cf37497e649b1771ed5957163a8690bee875ab
yaoqijun <yaoqijunmail@gmail.com>
2023-12-24 22:26:04 +0800

    add

---
+++ commands/cmd1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'yaoqijun'
+__mail__ = 'yaoqijunmail@foxmail.com'
+
+import click
+
+'''
+description: 
+'''
+
+
+@click.group(chain=True)
+def cli():
+    pass
+
+
+@cli.command('sdist')
+def sdist():
+    click.echo('sdist called')
+
+
+@cli.command('bdist_wheel')
+def bdist_wheel():
+    click.echo('bdist_wheel called')
